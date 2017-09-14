1cbe3b36240226abafa1d2ea449c968d9915d1a8
ABNG\chia <nwy140@gmail.com>
2017-09-14 16:49:04 +0800

    Remaking zoo app due to errors caused by android studio's update

---
+++ Kotlin/Zoo/Startup/app/src/main/java/silentwolfstudios/com/startup/Animal.kt
@@ -0,0 +1,18 @@
+package silentwolfstudios.com.startup
+
+import android.media.Image
+
+class Animal { //whenever you have an object, you will create an animal class of that object which stores details about that animal object
+
+    var name : String?=null;
+    var des : String?=null;
+    var image : Int? = null;
+
+    constructor(name:String , des:String, image: Int){
+        this.name=name;
+        this.des=des;
+        this.image=image;
+    }
+
+
+}
