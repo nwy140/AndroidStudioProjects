4a3c7ed55bc459d6ba1b8ccbcc5a202a58cf769e
LAPTOP-BUU174MN\AddictedToCoding <nwy140@gmail.com>
2017-09-22 14:05:05 +0800

    App Built not working for now

---
+++ Kotlin/Food App/Startup/app/src/main/java/silentwolfstudios/com/startup/Food.kt
@@ -0,0 +1,14 @@
+package silentwolfstudios.com.startup
+
+class Food{
+    var name:String?=null;
+    var des:String?=null;
+    var image:Int?=null;
+
+
+    constructor(name:String,des:String,Image:Int){
+        this.name=name;
+        this.des= des;
+        this.image=image;
+    }
+}
