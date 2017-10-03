3517330a75308eec3d19bf62c987117389cf2f6b
ABNG\chia <nwy140@gmail.com>
2017-10-03 12:48:16 +0800

    Activity for note writing created, button drawable added and applied to the activity's layout

---
+++ Kotlin/NoteApp/Startup/app/src/main/java/silentwolfstudios/com/startup/AddNotes.kt
@@ -0,0 +1,12 @@
+package silentwolfstudios.com.startup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNotes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notes)
+    }
+}
