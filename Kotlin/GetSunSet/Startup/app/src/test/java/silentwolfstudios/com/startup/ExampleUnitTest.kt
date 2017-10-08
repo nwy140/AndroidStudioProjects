6281d3cca0df85f2bccc84c2fade764de0bff5ce
LAPTOP-BUU174MN\AddictedToCoding <nwy140@gmail.com>
2017-10-08 10:46:28 +0800

    Nice Sunrise Layout Designed

--- Kotlin/GetSunSet/Startup/app/src/test/java/silentwolfstudios/com/startup/ExampleUnitTest.kt
+++ Kotlin/GetSunSet/Startup/app/src/test/java/silentwolfstudios/com/startup/ExampleUnitTest.kt
@@ -8,6 +8,28 @@ import org.junit.Assert.*
  * Example local unit test, which will execute on the development machine (host).
  *nwy
  * See [testing documentation](http://d.android.com/tools/testing).
+ 
+ udemy.com
+github.com
+raw.githubusercontent.com
+bit.ly
+coggle.it
+trello.com
+
+edx.org
+ref
+
+infinitelooper.com
+
+material.io
+developer.android.com
+stackoverflow.com
+
+
+ 
+ 
+ 
+ 
  */
 class ExampleUnitTest {
     @Test
