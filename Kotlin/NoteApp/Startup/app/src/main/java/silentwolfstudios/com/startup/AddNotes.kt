37b69784d6d15e91b52a57da553e9b5fcb036f9e
LAPTOP-BUU174MN\AddictedToCoding <nwy140@gmail.com>
2017-10-08 10:47:31 +0800

    SQLite Database implemented to work with kotlin, and take title and description from AddNote Activity

--- Kotlin/NoteApp/Startup/app/src/main/java/silentwolfstudios/com/startup/AddNotes.kt
+++ Kotlin/NoteApp/Startup/app/src/main/java/silentwolfstudios/com/startup/AddNotes.kt
@@ -1,17 +1,35 @@
 package silentwolfstudios.com.startup
-
+import android.content.ContentValues
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add_notes.*
 
 class AddNotes : AppCompatActivity() {
-
+    val  dbTable = "Notes";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
-    }
+        }
 
     fun buAdd(view: View){
-        finish(); //finish activity
+        var dbManager=DbManager(this);
+
+        var values = ContentValues();
+        values.put("Title",etTitle.text.toString() ); // take title from ettitle by colID from DbManager class
+        values.put("Description",etDes.text.toString() ); // take Description from ettitle by colID from DbManager class
+
+
+        var ID=dbManager.Insert(values)
+        if (ID>0){
+            Toast.makeText(this," note is added", Toast.LENGTH_LONG).show()
+            finish();
+        } else {
+            Toast.makeText(this," cannot add note", Toast.LENGTH_LONG).show()
+        }
+
+
+//        finish(); //finish activity
     }
 }
---
+++ Kotlin/NoteApp/Startup/app/src/main/java/silentwolfstudios/com/startup/DbManager.kt
@@ -0,0 +1,58 @@
+package silentwolfstudios.com.startup
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+class DbManager {
+
+    val dbName="MyNotes"
+    val dbTable="Notes"
+    val colID="ID"
+    val colTitle="Title"
+    val colDes="Description"
+    val dbVersion=1; // version number
+    //CREATE TABLE IF NOT EXISTS MyNotes (ID INTEGER PRIMARY KEY , title TEXT, Description TEXT);"
+    val sqlCreateTable="CREATE TABLE IF NOT EXISTS" + dbTable + "("+colID + "INTEGER PRIMARY KEY," +
+            colTitle + " TEXT, " + colDes + " TEXT):";
+    var sqlDB:SQLiteDatabase?=null;
+
+    //when application starts, constructor will call it
+    constructor(context: Context){
+        var db=DatabaseHelperNotes(context  ); // when database manager reated, it call helpernotes class, which will created the database that is not available.
+        sqlDB=db.writableDatabase;
+
+
+
+    }
+
+    //this SQLiteOpenHelper inherited class create database and execute commands in database
+    inner class DatabaseHelperNotes:SQLiteOpenHelper {
+
+        var context: Context? = null;
+
+        constructor(context: Context) : super(context, dbName, null, dbVersion) {
+            this.context = context;
+        }
+
+        override fun onCreate(p0: SQLiteDatabase?) {
+            p0!!.execSQL(sqlCreateTable) //sql statement  run
+            Toast.makeText(this.context, " database is created", Toast.LENGTH_LONG).show()
+            //when sql created show name of sql database
+        } //7867867857
+
+        //when someone update datapase version
+        override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+            p0!!.execSQL("Drop table IF EXIST" + dbTable)
+        }
+    }
+
+    fun Insert(values: ContentValues):Long{
+
+        val ID=sqlDB!!.insert(dbTable,"",values);
+        return ID;
+    }
+
+}
