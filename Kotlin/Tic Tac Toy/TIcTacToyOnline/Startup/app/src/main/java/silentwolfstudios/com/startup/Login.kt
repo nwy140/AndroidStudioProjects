add4edd9665227f246502877509cb9bda0fb5837
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-10-21 09:27:08 +0800

    TicTacToyOnline project created, firebase analytic implemented, Login activity created for email

---
+++ Kotlin/Tic Tac Toy/TIcTacToyOnline/Startup/app/src/main/java/silentwolfstudios/com/startup/Login.kt
@@ -0,0 +1,18 @@
+package silentwolfstudios.com.startup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import silentwolfstudios.com.startup.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    public fun buLoginEvent(view:View}){
+
+    }
+}
