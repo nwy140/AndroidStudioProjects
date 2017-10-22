96355f6a63ce77bfdd6ac2f402066443796389a8
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-10-23 06:08:30 +0800

    Email Log in is now passed to Online Firabase Database ,Added Function to split String to pass data into Firebase database

--- Kotlin/Tic Tac Toy/TIcTacToyOnline/Startup/app/src/main/java/silentwolfstudios/com/startup/Login.kt
+++ Kotlin/Tic Tac Toy/TIcTacToyOnline/Startup/app/src/main/java/silentwolfstudios/com/startup/Login.kt
@@ -1,5 +1,6 @@
 package silentwolfstudios.com.startup
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,12 +8,17 @@ import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 import silentwolfstudios.com.startup.R
 
 class Login : AppCompatActivity() {
 
     private var mAuth:FirebaseAuth?=null; //FireBase authentication
+
+    private var database=FirebaseDatabase.getInstance();
+    private var myRef=database.reference;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -30,6 +36,14 @@ class Login : AppCompatActivity() {
 
                 if (task.isSuccessful){
                     Toast.makeText(applicationContext,"Successful Login",Toast.LENGTH_LONG).show();
+
+                    var currentUser =mAuth!!.currentUser;
+
+                    //save in database
+                    if(currentUser!=null){
+                        myRef.child("Users").child(SplitString(currentUser.email.toString())).setValue(currentUser.uid) // you can't load email as name because Firebase can't load '@' char in their database
+
+                    }
 
                     LoadMain()
                 }else {
@@ -49,12 +63,20 @@ class Login : AppCompatActivity() {
         var currentUser =mAuth!!.currentUser;
 
         if(currentUser!=null){
+
+
+
             var intent=Intent(this, MainActivity::class.java);
             intent.putExtra("email",currentUser.email);
             intent.putExtra("email",currentUser.uid);
             startActivity(intent)
         }
 
+    }
+
+    fun SplitString(str:String):String{
+        var split=str.split("@")
+        return split[0]
     }
 
 
