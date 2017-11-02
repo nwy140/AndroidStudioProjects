a1f6018b1825796411f90413eb85cee50a8e6669
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-02 15:07:12 +0800

    Added function to play sounds and  sbProgress to move along currentposition of music. Added oncompletionlistener to restart audio after ending. Stop button stops audio

--- Kotlin/MediaPlayer/Startup/app/src/main/java/silentwolfstudios/com/startup/MainActivity.kt
+++ Kotlin/MediaPlayer/Startup/app/src/main/java/silentwolfstudios/com/startup/MainActivity.kt
@@ -1,5 +1,6 @@
 package silentwolfstudios.com.startup
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,12 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     var ListSongs=ArrayList<SongInfo>()
     var adapter:MySongAdapter?=null
+    var mp:MediaPlayer?=null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         LoadURLOnline()
         adapter=MySongAdapter(ListSongs)
         lsListSongs.adapter=adapter
+
+        var mytracking=mySongTrack()
+        mytracking.start()
     }
 
     fun LoadURLOnline(){
@@ -26,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         ListSongs.add(SongInfo("01 Drive","YokoShimomura","https://vocaroo.com/media_command.php?media=s1Cz7WMgL76d&command=download_mp3"))
         ListSongs.add(SongInfo("02 Critical Drive","YokoShimomura","https://vocaroo.com/media_command.php?media=s1Cz7WMgL76d&command=download_mp3"))
         ListSongs.add(SongInfo("03  C  ritical Drive","YokoShimomura","https://vocaroo.com/media_command.php?media=s1Cz7WMgL76d&command=download_mp3"))
-        ListSongs.add(SongInfo("04 Critical Drive","YokoShimomura","https://vocaroo.com/media_command.php?media=s1Cz7WMgL76d&command=download_mp3"))
+        ListSongs.add(SongInfo("04 wdrf Drive","YokoShimomura","http://soundbible.com/grab.php?id=2140&type=mp3"))
     }
 
     inner class MySongAdapter:BaseAdapter{
@@ -44,6 +50,24 @@ class MainActivity : AppCompatActivity() {
 
             myView.buPlay.setOnClickListener(View.OnClickListener {
                 //TODO: play Song
+            if(myView.buPlay.text=="STOP"){
+                mp!!.stop()
+                myView.buPlay.text="START"
+
+            }else{
+                mp=MediaPlayer()
+                try{
+                    mp!!.setDataSource(Song.SongURL   )
+                    mp!!.prepare()
+                    mp!!.start()
+                    myView.buPlay.text="STOP"
+                    sbProgress.max= mp!!.duration
+                    //restart music
+                    mp!!.setOnCompletionListener { mp!!.start() }
+
+                }catch (ex:Exception){}
+            }
+
             })
             return  myView
 
@@ -61,6 +85,24 @@ class MainActivity : AppCompatActivity() {
             return this.myListSong.size
         }
 
+    }
+    inner class mySongTrack():Thread(){
+
+        override fun run() {
+            while (true) {
+                try {
+                    Thread.sleep(1000)
+                } catch (ex: Exception) {
+                }
+
+                runOnUiThread {
+                    if (mp != null) {
+                        sbProgress.progress = mp!!.currentPosition
+                    }
+
+                }
+            }
+        }
     }
 
 }
