5561029925a3d56966524965cfc68228fbbf3478
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2017-11-02 18:34:33 +0800

    Nuclear Siren Alarm Audio added , the only option to turn siren off is by uninstalling the app

--- Kotlin/AlarmManager/Startup/app/src/main/java/silentwolfstudios/com/startup/myBroadcastReceiver.kt
+++ Kotlin/AlarmManager/Startup/app/src/main/java/silentwolfstudios/com/startup/myBroadcastReceiver.kt
@@ -12,11 +12,8 @@ class myBroadcastReceiver:BroadcastReceiver(){
             var b=intent.extras
 
             Toast.makeText(context,b.getString("message"),Toast.LENGTH_LONG).show()
-            var loopnotifyme:Boolean=true;
-
 val notifyme=Notifications();
-            while (loopnotifyme==true){notifyme.Notify(context!!,b.getString("message"),10)}
-
+            notifyme.Notify(context!!,b.getString("message"),10)
 
         }else if(intent!!.action.equals("android.intent.action.BOOT_COMPLETED")) { //system reset
             //TODO save time
